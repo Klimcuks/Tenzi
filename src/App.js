18939a69d49cacb94fe4558376cf26752755c974
Nikita Klimcuks <nikita.klimchuck@gmail.com>
2023-05-30 22:51:31 +0100

    rendering dice, added css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
+import "./style.css";
+import Die from "./components/Die";
+import { useState } from "react";
 
 function App() {
+  const [dice, setDice] = useState(allDice());
+
+  function allDice() {
+    const arrayOfNums = [];
+
+    for (let i = 0; i < 10; i++) {
+      let num = Math.floor(Math.random() * 6);
+      arrayOfNums.push(num);
+    }
+    return arrayOfNums;
+  };
+
+  const diceEl = dice.map((num) => <Die value={num} />);
+
+  function handleClick(){
+    setDice(allDice())
+  }
+
   return (
-    <div className="App">
-      
-    </div>
+    <main>
+      <div className="dice-container">{diceEl}</div>
+      <button className="roll-btn" onClick={handleClick}>Roll</button>
+    </main>
   );
 }
 
